94d193c80b4f8c616fbd15ff3daba5d54ce176e4
bdomingu <beatrizdominguez14@gmail.com>
2023-08-15 17:39:00 -0600

    fixed env for cloudsql

--- pages/api/db.tsx
+++ pages/api/db.tsx
@@ -4,7 +4,7 @@ const sequelize = new Sequelize({
   database: process.env.NEXT_PUBLIC_DATABASE,
   username: process.env.NEXT_PUBLIC_USER,
   password: process.env.NEXT_PUBLIC_PASSWORD,
-  host: `/cloudsql/${process.env.NEXT_PUBLIC_HOST}`,
+  host: process.env.NEXT_PUBLIC_HOST === 'localhost' ? "http://localhost:3000" : `/cloudsql/${process.env.NEXT_PUBLIC_HOST}`,
   port: 3306, 
   dialect: 'mysql',
   dialectOptions: {
