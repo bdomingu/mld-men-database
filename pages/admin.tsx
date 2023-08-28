113a192aca5a32ec20a73910d8756d43a69cccb7
bdomingu <beatrizdominguez14@gmail.com>
2023-08-28 15:56:45 -0600

    fixed loading for admin page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -3,26 +3,35 @@ import { useState, useEffect} from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import Layout from "@/components/Layout";
+import { useRouter } from 'next/router';
 import fetchAdminStatus from "@/components/AdminStatus";
 import Invalid from '@/components/Invalid';
+import Loading from '@/components/Loading';
 
 
 const Admin = () => {
     const [expirationTime, setExpirationTime] = useState(1);
     const [registrationLink, setRegistrationLink] = useState('');
-    const [isAdminUser, setIsAdminUser] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const admin = Cookies.get('user');
+    const router = useRouter();
+
     
     
     useEffect(() => {
         const checkAdminStatus = async () => {
                const isAdmin = await fetchAdminStatus();
-               setIsAdminUser(isAdmin);
+            if(!isAdmin){
+                router.replace('/unauthorized')
+            } else {
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 2000);
+            }
            } 
            checkAdminStatus()
-    }, [])
+    }, [router])
 
-   
 
     const generateLink = async () => {
 
@@ -56,30 +65,31 @@ const Admin = () => {
 
     return (
         <Layout footerColor="black">
-            {isAdminUser ? (
-              <div className={styles.container}>
-              <h4>Enter a number of day(s) you would like to set for the registration link. Please copy and paste the link somewhere safe as it will disappear on refresh.</h4>
-              <div className={styles.inputContainer}>
-              <label>
-                  Expiration Time (days):
-                  <input
-                  type="number"
-                  min="1"
-                  value={expirationTime}
-                  onChange={handleExpirationChange}
-                  />
-              </label>
-              <button onClick={generateLink}>Generate</button>
-              </div>
-              <div className={styles.registrationLink}>
-                <div className={styles.link}>
-                <p >{registrationLink}</p>
-                </div>
-              </div>
-          </div>
+            {isLoading ? (
+                <Loading/>
             ) : (
-            <Invalid/>
+                <div className={styles.container}>
+                <h4>Enter a number of day(s) you would like to set for the registration link. Please copy and paste the link somewhere safe as it will disappear on refresh.</h4>
+                <div className={styles.inputContainer}>
+                <label>
+                    Expiration Time (days):
+                    <input
+                    type="number"
+                    min="1"
+                    value={expirationTime}
+                    onChange={handleExpirationChange}
+                    />
+                </label>
+                <button onClick={generateLink}>Generate</button>
+                </div>
+                <div className={styles.registrationLink}>
+                  <div className={styles.link}>
+                  <p >{registrationLink}</p>
+                  </div>
+                </div>
+            </div>
             )}
+         
         </Layout>
     );
 }
